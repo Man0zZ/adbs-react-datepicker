01b51455a7b18ea41fb297e6c09b3c638057cb25
Man0zZ <manoj.shrestha@yipl.com>
2025-12-23 14:06:50 +0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  base: '/adbs-react-datepicker/',
   plugins: [
     react(),
     dts({
